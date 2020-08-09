b48c598f16ac040e6f220a4175709ec82e76195f
Razi D <racieldiz@gmail.com>
2020-08-08 20:25:52 -0700

    configuration
    adding api keys and others

---
+++ chatapp/src/components/config.js
@@ -0,0 +1,5 @@
+export default {
+    appId: "219340e9fae7d4d", //Enter your App ID
+    apiKey: "a6aec877cf85d75792d2e9228db413fd80a334a8", //Enter your API KEY
+    GUID: "supergroup", // Enter your group UID
+  };
